17eb46bd10ea02649514753e34e4cb069ef5e2ad
mohammadreza <mohammad.76kiani@gmail.com>
2023-11-18 11:57:13 -0700

    Update bash script

--- runner.sh
+++ runner.sh
@@ -1,5 +1,9 @@
 !/bin/bash
 
-for ((i=0;i<=199;i++)); do
-    python3 twitter/twitter-following-extractor.py $i
+for ((i=0;i<=10;i++)); do
+    python3 twitter/twitter-tweets-crawler.py $i
+    if [ $((i % 10)) -eq 0 ]; then
+        echo "Sleeping for 10 minutes..."
+        sleep 600  # 600 seconds = 10 minutes
+    fi
 done
