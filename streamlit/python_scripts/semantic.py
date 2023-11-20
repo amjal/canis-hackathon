a58a2f41ec6076c039e6aed7ee28fed5af022d13
mohammadreza <mohammad.76kiani@gmail.com>
2023-11-19 23:22:17 -0700

    Modify code

--- streamlit/python_scripts/semantic.py
+++ streamlit/python_scripts/semantic.py
@@ -1,16 +1,22 @@
 from bertopic import BERTopic
 import pandas as pd
-topic_model = BERTopic.load("./topic.pkl")
-df = pd.read_csv("../../twitter/text/tweets_with_topic_with_loc.csv")
+
+topic_model = BERTopic.load("../twitter/text/topic.pkl", embedding_model="all-mpnet-base-v2")
+df = pd.read_csv("../twitter/text/tweets_with_topic_loc.csv")
+
 
 class Agent:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def get_locations(self, text):
-		similar_topics, similarity = topic_model.find_topics(text, top_n=3)
-		topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
-		topic_names = [t.values[0] for t in topic_names]
-		return df[df["topic"].isin(topic_names)]['screen_name']
+    def get_locations(self, text):
+        similar_topics, similarity = topic_model.find_topics(text, top_n=3)
+        topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
+        topic_names = [t.values[0] for t in topic_names]
+        return df[df["topic"].isin(topic_names)]['location']
 
-
+    def get_users(self, text):
+        similar_topics, similarity = topic_model.find_topics(text, top_n=3)
+        topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
+        topic_names = [t.values[0] for t in topic_names]
+        return df[df["topic"].isin(topic_names)]['location']
