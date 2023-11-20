816390d6b7f50a8ba45f54d9fb39c935668e3288
amir jalili <jalilia370@gmail.com>
2023-11-20 00:41:41 -0700

    word cloud added

--- streamlit/main.py
+++ streamlit/main.py
@@ -5,12 +5,15 @@ import pandas as pd
 import streamlit as st
 from python_scripts import TwitterContent, CanisContent, FollowGraph, PlotlyAgent, WorldMapAgent, WikiGraph, semantic
 
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 # Streamlit app layout
 st.set_page_config(layout="wide")
 st.title("📊 CANIS DataViz Challenge | Hackathon 2023")
 
 tab_home, tab_canis, tab_twitter_content, tab_network = st.tabs(
-    ["🏠 Home", "📊 Canis Data Analysis", "🐦 Twitter Content Analysis", "🌐 Following Graph"]
+	["🏠 Home", "📊 Canis Data Analysis", "🐦 Twitter Content Analysis", "🌐 Following Graph"]
 )
 canis_agent = CanisContent.Agent()
 geo_agent = WorldMapAgent.Agent("../twitter/users/", "../twitter/followers/")
@@ -22,115 +25,131 @@ networx_agent = WikiGraph.Agent()
 
 # Page content based on navigation
 with tab_home:
-    # Homepage title
-    st.title("🔍 CANIS DataViz Hackathon Project by Team Insightful Four")
+	# Homepage title
+	st.title("🔍 CANIS DataViz Hackathon Project by Team Insightful Four")
 
-    # Brief Introduction
-    st.markdown("""
-    Welcome to our project dashboard! We're Team Insightful Four, comprised of Alireza, Morteza, Amir, and Mohammad Reza, 
-    bringing together our diverse skills to tackle the CANIS Data Visualization Challenge. 🚀
-    """)
+	# Brief Introduction
+	st.markdown("""
+	Welcome to our project dashboard! We're Team Insightful Four, comprised of Alireza, Morteza, Amir, and Mohammad Reza, 
+	bringing together our diverse skills to tackle the CANIS Data Visualization Challenge. 🚀
+	""")
 
-    # About the Challenge
-    st.header("🎯 About Our Challenge")
-    st.markdown("""
-    Diving deep into data, our mission was to transform a raw dataset into a captivating visual narrative. 
-    We began by dissecting the CANIS data, seeking patterns and stories hidden within. Our journey took a 
-    twist as we embarked on scraping Twitter, gleaning insights from the web of interactions and content 
-    created by the users provided to us.
-    """)
+	# About the Challenge
+	st.header("🎯 About Our Challenge")
+	st.markdown("""
+	Diving deep into data, our mission was to transform a raw dataset into a captivating visual narrative. 
+	We began by dissecting the CANIS data, seeking patterns and stories hidden within. Our journey took a 
+	twist as we embarked on scraping Twitter, gleaning insights from the web of interactions and content 
+	created by the users provided to us.
+	""")
 
-    # Our Process
-    st.header("🔬 Our Analytical Process")
-    st.markdown("""
-    Our approach was systematic and exploratory:
-    - **Data Analysis**: Initiated with a thorough analysis of CANIS data.
-    - **Twitter Scraping**: Progressed to scraping Twitter for user profiles, their networks, and recent tweets.
-    - **Insight Extraction**: Delved into the data to uncover trends, anomalies, and noteworthy findings.
-    - **Visualization**: Transformed our discoveries into an array of engaging, insightful visuals.
-    """)
+	# Our Process
+	st.header("🔬 Our Analytical Process")
+	st.markdown("""
+	Our approach was systematic and exploratory:
+	- **Data Analysis**: Initiated with a thorough analysis of CANIS data.
+	- **Twitter Scraping**: Progressed to scraping Twitter for user profiles, their networks, and recent tweets.
+	- **Insight Extraction**: Delved into the data to uncover trends, anomalies, and noteworthy findings.
+	- **Visualization**: Transformed our discoveries into an array of engaging, insightful visuals.
+	""")
 
-    # Submission Requirements
-    st.header("📑 Submission Highlights")
-    st.markdown("""
-    In our submission, you'll find:
-    - **Structured Presentation**: A cohesive narrative of our data journey, from raw figures to polished graphs.
-    - **Methodology Summary**: Insight into the analytical tools and techniques we employed.
-    - **Codebase**: Access to the code that powered our analysis, showcasing our technical acumen.
-    """)
+	# Submission Requirements
+	st.header("📑 Submission Highlights")
+	st.markdown("""
+	In our submission, you'll find:
+	- **Structured Presentation**: A cohesive narrative of our data journey, from raw figures to polished graphs.
+	- **Methodology Summary**: Insight into the analytical tools and techniques we employed.
+	- **Codebase**: Access to the code that powered our analysis, showcasing our technical acumen.
+	""")
 
-    # Final Thoughts
-    st.header("💡 Parting Thoughts")
-    st.markdown("""
-    Whether you're a data enthusiast, a visualization whiz, or just curious about the power of data, we invite you to 
-    explore our findings. Together, let's celebrate the fusion of data, creativity, and technology!
-    """)
+	# Final Thoughts
+	st.header("💡 Parting Thoughts")
+	st.markdown("""
+	Whether you're a data enthusiast, a visualization whiz, or just curious about the power of data, we invite you to 
+	explore our findings. Together, let's celebrate the fusion of data, creativity, and technology!
+	""")
 
-    # Footer
-    st.markdown("---")
-    st.markdown("""
-    Made with ❤️ by Team Insightful Four: Alireza, Morteza, Amir, and Mohammad Reza.
-    """)
+	# Footer
+	st.markdown("---")
+	st.markdown("""
+	Made with ❤️ by Team Insightful Four: Alireza, Morteza, Amir, and Mohammad Reza.
+	""")
 
 with tab_canis:
-    fig = canis_agent.show_distribution_of_social_media()
-    st.plotly_chart(fig)
-    fig = canis_agent.show_distribution_of_parent_entities()
-    st.plotly_chart(fig)
-    fig = canis_agent.show_distribution_of_records_per_parents()
-    st.plotly_chart(fig)
+	fig = canis_agent.show_distribution_of_social_media()
+	st.plotly_chart(fig)
+	fig = canis_agent.show_distribution_of_parent_entities()
+	st.plotly_chart(fig)
+	fig = canis_agent.show_distribution_of_records_per_parents()
+	st.plotly_chart(fig)
 
-    col1, col2 = st.columns(2)
+	col1, col2 = st.columns(2)
 
-    user_locs = geo_agent.cities2coords_cached(geo_agent.users2cities(), "../data/loc2coord.json")
-    user_locs = geo_agent.generate_pointmap(array=user_locs)
-    col1.write("📍 User Geolocations: A Visual Map of the Community. Each pinpoint reflects a unique user in Canis dataset, demonstrating the expansive reach of our network across the globe.")
-    col1.map(user_locs)
+	user_locs = geo_agent.cities2coords_cached(geo_agent.users2cities(), "../data/loc2coord.json")
+	user_locs = geo_agent.generate_pointmap(array=user_locs)
+	col1.write("📍 User Geolocations: A Visual Map of the Community. Each pinpoint reflects a unique user in Canis dataset, demonstrating the expansive reach of our network across the globe.")
+	col1.map(user_locs)
 
-    following_locs = geo_agent.cities2coords_cached(geo_agent.following2cities(), "../data/loc2coord.json")
-    following_locs = geo_agent.generate_pointmap(array=following_locs)
-    col2.write("📍 The same visual map for the followings of Canis users.")
-    col2.map(following_locs)
+	following_locs = geo_agent.cities2coords_cached(geo_agent.following2cities(), "../data/loc2coord.json")
+	following_locs = geo_agent.generate_pointmap(array=following_locs)
+	col2.write("📍 The same visual map for the followings of Canis users.")
+	col2.map(following_locs)
 
-    st.write("Parent Entity vs Region of Focus Concentration")
-    fig = plotly_agent.plot_3d_heat('Entity owner (English)', 'Region of Focus', 10)
-    st.plotly_chart(fig)
+	st.write("Parent Entity vs Region of Focus Concentration")
+	fig = plotly_agent.plot_3d_heat('Entity owner (English)', 'Region of Focus', 10)
+	st.plotly_chart(fig)
 
 with tab_twitter_content:
-    fig = time_series_agent.show_hashtags_plot()
-    st.plotly_chart(fig)
-    fig = time_series_agent.show_hashtags_trend()
-    st.plotly_chart(fig)
-    fig = time_series_agent.show_topics_trend()
-    st.plotly_chart(fig)
-    fig = time_series_agent.show_topics_per_parent()
-    st.plotly_chart(fig)
-    fig = time_series_agent.show_most_impactful_topics()
-    st.plotly_chart(fig)
+	fig = time_series_agent.show_hashtags_plot()
+	st.plotly_chart(fig)
+	fig = time_series_agent.show_hashtags_trend()
+	st.plotly_chart(fig)
+	fig = time_series_agent.show_topics_trend()
+	st.plotly_chart(fig)
+	fig = time_series_agent.show_topics_per_parent()
+	st.plotly_chart(fig)
+	fig = time_series_agent.show_most_impactful_topics()
+	st.plotly_chart(fig)
 
-    user_input = st.text_input("What do you want?")
-    cities = semantic_agent.get_locations(user_input)
-    locs = geo_agent.cities2coords_cached(cities, "../data/loc2coord.json")
-    r = geo_agent.generate_heatmap_by_country("../data/custom.geo.json", locs)
-    st.pydeck_chart(r)
+	user_input = st.text_input("What do you want?")
+	col1, col2 = st.columns(2)
+	cities = semantic_agent.get_locations(user_input)
+	locs = geo_agent.cities2coords_cached(cities, "../data/loc2coord.json")
+	r = geo_agent.generate_heatmap_by_country("../data/custom.geo.json", locs)
+	col1.pydeck_chart(r)
+
+	# Word Cloud
+	tweets = "".join(semantic_agent.get_clean_tweets(user_input).values)
+	wordcloud = WordCloud(max_font_size=40).generate(tweets)
+	fig, ax = plt.subplots(figsize = (12, 8))
+	ax.imshow(wordcloud)
+	plt.axis("off")
+	col2.pyplot(fig)
+	'''
+	plt.figure()
+	plt.imshow(wordcloud, interpolation="bilinear")
+	plt.axis("off")
+	plt.show()
+	'''
+
 
 with tab_network:
-    st.write("Following Graph")
-    df = pd.read_csv('../3d-network-visualization/clean_csvs/network.csv')
-    text = st.text_input('Enter a text to analyse', 'all')
-    if text == 'all':
-        users = None
-    else:
-        users = semantic_agent.get_users(text)
-    user_parent_entity = st.multiselect('select user parent entity', df['user_parent_entity'].unique())
-    col1, col2 = st.columns(2)
-    following_parent_entity = col1.multiselect('Select following parent entity', df['following_parent_entity'].unique())
-    fig = following_graph_agent.plot_network_graph(user_parent_entity, following_parent_entity, users)
-    col1.plotly_chart(fig, use_container_width=True)
-    following_parent_entity_2 = col2.multiselect('Select following parent entity to compare',
-                                                 df['following_parent_entity'].unique())
-    fig2 = following_graph_agent.plot_network_graph(user_parent_entity, following_parent_entity_2)
-    col2.plotly_chart(fig2, use_container_width=True)
-    st.write("Wiki Network Graph")
-    graph = networx_agent.plot_network_graph()
-    st.components.v1.html(graph, height=800, width=800, scrolling=True)
+	st.write("Following Graph")
+	df = pd.read_csv('../3d-network-visualization/clean_csvs/network.csv')
+	text = st.text_input('Enter a text to analyse', 'all')
+	if text == 'all':
+		users = None
+	else:
+		users = semantic_agent.get_users(text)
+	user_parent_entity = st.multiselect('select user parent entity', df['user_parent_entity'].unique())
+	col1, col2 = st.columns(2)
+	following_parent_entity = col1.multiselect('Select following parent entity', df['following_parent_entity'].unique())
+	fig = following_graph_agent.plot_network_graph(user_parent_entity, following_parent_entity, users)
+	col1.plotly_chart(fig, use_container_width=True)
+	following_parent_entity_2 = col2.multiselect('Select following parent entity to compare',
+												 df['following_parent_entity'].unique())
+	fig2 = following_graph_agent.plot_network_graph(user_parent_entity, following_parent_entity_2)
+	col2.plotly_chart(fig2, use_container_width=True)
+	st.write("Wiki Network Graph")
+	graph = networx_agent.plot_network_graph()
+	st.components.v1.html(graph, height=800, width=800, scrolling=True)
--- streamlit/python_scripts/semantic.py
+++ streamlit/python_scripts/semantic.py
@@ -1,22 +1,41 @@
 from bertopic import BERTopic
 import pandas as pd
+import re
+import emoji
 
 topic_model = BERTopic.load("../twitter/text/topic.pkl", embedding_model="all-mpnet-base-v2")
 df = pd.read_csv("../twitter/text/tweets_with_topic_loc.csv")
 
+def clean_tweet_df(row):
+    tweet = row["full_text"]
+    tweet = re.sub("@[A-Za-z0-9]+","",tweet) #Remove @ sign
+    tweet = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", "", tweet) #Remove http links
+    tweet = " ".join(tweet.split())
+    tweet = ''.join(c for c in tweet if c not in emoji.EMOJI_DATA)
+    tweet = tweet.replace("#", "").replace("_", " ").replace("RT ", "").replace('&amp;', '&') #Remove hashtag sign but keep the text
+    return tweet
+
+df["cleaned_full_text"] = df.apply(clean_tweet_df, axis=1)
 
 class Agent:
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def get_locations(self, text):
-        similar_topics, similarity = topic_model.find_topics(text, top_n=3)
-        topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
-        topic_names = [t.values[0] for t in topic_names]
-        return df[df["topic"].isin(topic_names)]['location']
+	def get_locations(self, text):
+		similar_topics, similarity = topic_model.find_topics(text, top_n=3)
+		topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
+		topic_names = [t.values[0] for t in topic_names]
+		return df[df["topic"].isin(topic_names)]['location']
 
-    def get_users(self, text):
-        similar_topics, similarity = topic_model.find_topics(text, top_n=3)
-        topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
-        topic_names = [t.values[0] for t in topic_names]
-        return df[df["topic"].isin(topic_names)]['location']
+	def get_users(self, text):
+		similar_topics, similarity = topic_model.find_topics(text, top_n=3)
+		topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
+		topic_names = [t.values[0] for t in topic_names]
+		return df[df["topic"].isin(topic_names)]['location']
+	
+	def get_clean_tweets(self, text):
+		similar_topics, similarity = topic_model.find_topics(text, top_n=3)
+		topic_names = [topic_model.get_topic_info(t)["Name"] for t in similar_topics]
+		topic_names = [t.values[0] for t in topic_names]
+		return df[df["topic"].isin(topic_names)]["cleaned_full_text"]
+
